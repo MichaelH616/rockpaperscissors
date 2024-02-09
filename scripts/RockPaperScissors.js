d1ab8f00dd105df371a362c0a16b7a4d1635a3b2
MichaelH616 <michael_gets_mail@yahoo.com>
2024-02-09 14:04:57 -0800

    added eventlisteners and keydown function to play with keyboard types

--- scripts/RockPaperScissors.js
+++ scripts/RockPaperScissors.js
@@ -16,8 +16,32 @@ score = {
 }
 */
 
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  }
+
+});
+
 function playGame(playerMove) {
 const computerMove = pickComputerMove();
+
 let result = '';
     
 if(playerMove === 'scissors') {
@@ -107,7 +131,7 @@ function autoPlay() {
   }
 
   if (!isAutoPlaying) {
-   intervalId = setInterval(function() {
+   intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 2000);
